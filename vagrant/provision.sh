855b452c802d4155ce49d231f7dd43ad82158dfa
Dušan Simić <dusan.simic1810@gmail.com>
2024-02-09 17:47:58 +0100

    feat: Add bundle script to provision

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -4,4 +4,5 @@ set -e
 curl -L https://github.com/jqlang/jq/releases/latest/download/jq-linux-amd64 --create-dirs --output "/usr/local/bin/jq"
 curl -L https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 --create-dirs --output "/usr/local/bin/yq"
 curl -L https://raw.githubusercontent.com/dusansimic/fp-build/main/fp-build.sh --create-dirs --output "/usr/local/bin/fp-build"
-chmod u=rwx,g=rx,o=rx /usr/local/bin/jq /usr/local/bin/yq /usr/local/bin/fp-build
+curl -L https://raw.githubusercontent.com/dusansimic/fp-build/main/fp-bundle.sh --create-dirs --output "/usr/local/bin/fp-bundle"
+chmod u=rwx,g=rx,o=rx /usr/local/bin/jq /usr/local/bin/yq /usr/local/bin/fp-build /usr/local/bin/fp-bundle
